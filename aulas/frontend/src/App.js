840c3326745ca1c2ba89d4c37687e617518af7a6
Elza Simões <elzaesimoes@gmail.com>
2020-04-04 19:09:44 -0400

    Finalizando Register e Logon page

--- aulas/frontend/src/App.js
+++ aulas/frontend/src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import Routes from './routes';
+
+import './global.css'
 
 function App() {
+
   return (
-   <h1>Hello, World!</h1>
+  <div>
+  <Routes />
+  </div>
+  
   );
 }
 
