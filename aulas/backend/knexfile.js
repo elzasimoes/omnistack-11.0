505b770f3081629fe4157c27c1916b995b5075d9
Elza Simões <elzaesimoes@gmail.com>
2020-04-05 18:03:39 -0400

    Frontend Finalizado

--- aulas/backend/knexfile.js
+++ aulas/backend/knexfile.js
@@ -7,19 +7,8 @@ module.exports = {
     connection: {
       filename: './src/database/db.sqlite'
     },
-    migrations:{
-      directory: "./src/database/migrations"
-    },
-    useNullAsDefault: true,
-  },
-
-  test: {
-    client: 'sqlite3',
-    connection: {
-      filename: './src/database/test.sqlite'
-    },
-    migrations:{
-      directory: "./src/database/migrations"
+    migrations: {
+      directory: './src/database/migrations'
     },
     useNullAsDefault: true,
   },
