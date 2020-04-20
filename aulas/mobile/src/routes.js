67ff2615fb061e9daf25f0b76290cbdebba661d3
Elza Simões <elzaesimoes@gmail.com>
2020-04-20 10:58:19 -0400

    issue #1 fixed

--- aulas/mobile/src/routes.js
+++ aulas/mobile/src/routes.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native'
-import { CreateStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-const AppStack = CreateStackNavigator();
+const AppStack = createStackNavigator();
+
 import Incidents from './pages/Incidents';
 import Detail from './pages/Detail';
 
 export default function Routes() {
-    return(
-        <NavigationContainer>
-            <AppStack.Navigator screenOptions = {{ headerShown: false }}>
-                <AppStack.Screen component ={Incidents}/>
-                <AppStack.Screen component ={Detail}/>
-            </AppStack.Navigator>
-        </NavigationContainer>
-    );
-}
+  return (
+    <NavigationContainer>
 
+      <AppStack.Navigator screenOptions={{ headerShown: false }}>
+        <AppStack.Screen name="Incidents" component={Incidents} />
+        <AppStack.Screen name="Detail" component={Detail} />
+      </AppStack.Navigator>
+
+    </NavigationContainer>
+  );
+}
