4c071b820149836f546942cfa1e1dff5fa0123ff
Elza Simões <elzaesimoes@gmail.com>
2020-04-20 15:10:32 -0400

    finalizado

--- aulas/mobile/App.js
+++ aulas/mobile/App.js
@@ -1,11 +1,12 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Routes from './src/routes'
+
+import Routes from './src/routes';
 
 export default function App() {
   return (
     <Routes />
   );
 }
-
-
